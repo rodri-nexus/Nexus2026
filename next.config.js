9e6bdf77883b359430cac7b0ed4cf8b253eb9602
rodri-nexus <hogarnexu@gmail.com>
2026-08-01 11:28:15 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.tiendanube.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.mitiendanube.com",
+      },
+      {
+        protocol: "https",
+        hostname: "http2.mlstatic.com",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
