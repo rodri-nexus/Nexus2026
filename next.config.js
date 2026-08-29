ae86d29dab0677ab66104965b5be585d7cd7b179
rodri-nexus <hogarnexu@gmail.com>
2026-08-29 19:31:54 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -17,6 +17,28 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        // Forzar a todas las tiendas y navegadores a descargar siempre la última versión del widget
+        source: "/nevux-widget.js",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, OPTIONS",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
